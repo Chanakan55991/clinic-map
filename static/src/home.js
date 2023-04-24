a246e9a0af4640a36cdb77fbc6580cc30fbad9f8
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-04-24 17:02:08 +0700

    converted to signals in preact

--- static/src/home.js
+++ static/src/home.js
@@ -1,6 +1,7 @@
 import { html } from './html.js'
 import { getUserLocation } from './utils/userlocation.js'
 import { signal } from 'https://esm.sh/@preact/signals'
+import { useEffect } from 'https://esm.sh/preact/hooks'
 
 const greenIcon = L.icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
@@ -23,54 +24,53 @@ const redIcon = L.icon({
 let markers = []
 const usrLocation = await getUserLocation()
 const places = ['hospital', 'clinic']
+const radius = signal(5000)
 
-const getMapData = async (radius, map) => {
-  markers.forEach(marker => marker.remove())
-  markers = []
-  const p = places.join('|')
+let map = L.map('map').setView([usrLocation.coords.latitude, usrLocation.coords.longitude], 15); // could find a way to modify this 15 number to modify zoom automaticcally
+let circle = L.circle([usrLocation.coords.latitude, usrLocation.coords.longitude], {
+  color: 'blue',
+  radius: 5000
+}).addTo(map)
 
-  const nodeQuery = `node["amenity"~"${p}"](around:${radius},${usrLocation.coords.latitude},${usrLocation.coords.longitude});`
-  const query = '?data=[out:json][timeout:15];(' + nodeQuery + ');out;'
-
-  const res = await fetch(`https://www.overpass-api.de/api/interpreter${query}`)
-  const result = await res.json()
-
-  result.elements.forEach(place => {
-    let icon
-    if ((place.tags.name && place.tags.name.toLowerCase().includes('clinic')) || (place.tags.clinic) || (place.tags.amenity.toLowerCase().includes('clinic'))) {
-      icon = greenIcon
-    } else if ((place.tags.name && place.tags.name.toLowerCase().includes('hospital')) || (place.tags.hospital) || (place.tags.amenity.toLowerCase().includes('hospital'))) {
-      icon = redIcon
-    }
-    console.log(icon)
-    let marker = L.marker([place.lat, place.lon], { icon: icon }).addTo(map);
-    markers.push(marker)
-  })
-}
-
-const Home = async () => {
-  let radius = 5000
-
-  let map = L.map('map').setView([usrLocation.coords.latitude, usrLocation.coords.longitude], 15); // could find a way to modify this 15 number to modify zoom automaticcally
+const Home = () => {
   const radiusButtons = ['rad_5km', 'rad_10km', 'rad_20km'];
+  useEffect(() => {
+    circle.setRadius(radius.value)
+    map.setView([usrLocation.coords.latitude, usrLocation.coords.longitude], Math.round(15 - Math.log(radius.value / 500) / Math.LN2));
+  }, [radius.value])
 
-  let circle = L.circle([usrLocation.coords.latitude, usrLocation.coords.longitude], {
-    color: 'blue',
-    radius: 5000
-  }).addTo(map)
+  useEffect(async () => {
+    markers.forEach(marker => marker.remove())
+    markers = []
+    const p = places.join('|')
+
+    const nodeQuery = `node["amenity"~"${p}"](around:${radius.value},${usrLocation.coords.latitude},${usrLocation.coords.longitude});`
+    const query = '?data=[out:json][timeout:15];(' + nodeQuery + ');out;'
+
+    const res = await fetch(`https://www.overpass-api.de/api/interpreter${query}`)
+    const result = await res.json()
+
+    result.elements.forEach(place => {
+      let icon
+      if ((place.tags.name && place.tags.name.toLowerCase().includes('clinic')) || (place.tags.clinic) || (place.tags.amenity.toLowerCase().includes('clinic'))) {
+        icon = greenIcon
+      } else if ((place.tags.name && place.tags.name.toLowerCase().includes('hospital')) || (place.tags.hospital) || (place.tags.amenity.toLowerCase().includes('hospital'))) {
+        icon = redIcon
+      }
+      let marker = L.marker([place.lat, place.lon], { icon: icon }).addTo(map);
+      markers.push(marker)
+    })
+  }, [radius.value])
 
   radiusButtons.forEach(buttonId => {
+    markers.forEach(marker => marker.remove())
+
     markers = []
     const button = document.getElementById(buttonId);
     button.addEventListener('click', async () => {
-      const rad = buttonId === 'rad_5km' ? 5000 :
+      radius.value = buttonId === 'rad_5km' ? 5000 :
         buttonId === 'rad_10km' ? 10000 :
           buttonId === 'rad_20km' ? 20000 : 0
-
-      radius = rad
-      getMapData(rad, map)
-      circle.setRadius(rad)
-      map.setView([usrLocation.coords.latitude, usrLocation.coords.longitude], Math.round(15 - Math.log(rad / 500) / Math.LN2));
     });
   });
 
@@ -78,8 +78,6 @@ const Home = async () => {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
-
-  getMapData(radius, map)
 
   return html`
 <h1 style=>Maps</h1>
